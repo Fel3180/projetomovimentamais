26eabfb6836b3538dcfb17673d6fb16cf16e0b81
Felipe Oliveira <Fel_oliveira_silva@yahoo.com.br>
2023-10-01 10:40:59 -0300

    cadastro de usuario

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("com.android.application")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -12,7 +13,7 @@ android {
         targetSdk = 33
         versionCode = 1
         versionName = "1.0"
-
+        vectorDrawables.useSupportLibrary = true
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
@@ -29,6 +30,9 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -39,4 +43,10 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
+    implementation("com.google.firebase:firebase-analytics-ktx")
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-firestore")
+
+
 }
--- app/src/main/java/com/example/projetomovimenta/FormLogin.java
+++ app/src/main/java/com/example/projetomovimenta/FormLogin.java
@@ -1,18 +1,55 @@
 package com.example.projetomovimenta;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.util.Objects;
 
 public class FormLogin extends AppCompatActivity {
+
+    private TextView text_tela_cadastro;
+    private EditText edit_email,edit_senha;
+    private Button bt_entrar;
+    private ProgressBar progressBar;
+    String[]mensagens ={"Preencha todos os campos"};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_login);
 
-//        Objects.requireNonNull(getSupportActionBar()).hide();
+       //getSupportActionBar().hide();
+        IniciarComponentes();
+        text_tela_cadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent= new Intent(FormLogin.this, FormCadastro.class);
+                startActivity(intent);
+            }
+        });
+    }
+    private void IniciarComponentes(){
+        text_tela_cadastro=findViewById(R.id.text_tela_cadastro);
+
     }
 }
