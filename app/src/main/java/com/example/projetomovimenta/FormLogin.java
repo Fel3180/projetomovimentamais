a996d3e0cb7e5847fb7b6af97eb8a95afe1e87f3
Felipe Oliveira <Fel_oliveira_silva@yahoo.com.br>
2023-10-03 20:18:13 -0300

    Tres telas funcionando

--- app/src/main/java/com/example/projetomovimenta/FormLogin.java
+++ app/src/main/java/com/example/projetomovimenta/FormLogin.java
@@ -1,8 +1,8 @@
 package com.example.projetomovimenta;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
@@ -10,17 +10,22 @@ import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.Toast;
-
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-
+import com.google.firebase.auth.GoogleAuthProvider;
 import java.util.Objects;
 
 public class FormLogin extends AppCompatActivity {
@@ -29,6 +34,10 @@ public class FormLogin extends AppCompatActivity {
     private EditText edit_email, edit_senha;
     private Button bt_entrar;
     private ProgressBar progressBar;
+    private FirebaseAuth mAuth;
+    private GoogleSignInClient mGoogleSignInClient;
+    private ImageView btnGoogleLogin;
+    private static final int RC_SIGN_IN = 123;
     String[] mensagens = {"Preencha todos os campos"};
 
     @Override
@@ -36,10 +45,10 @@ public class FormLogin extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_login);
 
-        //getSupportActionBar().hide();
+        mAuth = FirebaseAuth.getInstance();
+
         IniciarComponentes();
         text_tela_cadastro.setOnClickListener(v -> {
-
             Intent intent = new Intent(FormLogin.this, FormCadastro.class);
             startActivity(intent);
         });
@@ -54,41 +63,59 @@ public class FormLogin extends AppCompatActivity {
                 snackbar.setTextColor(Color.BLACK);
                 snackbar.show();
             } else {
-                AutenticarUsuario(v);
+                AutenticarUsuario(email, senha);
             }
         });
+
+        // Configurar o clique no botão de login do Google
+        btnGoogleLogin.setOnClickListener(view -> signInWithGoogle());
+
+
+        // Configurar as opções do login do Google
+
     }
 
-    private void AutenticarUsuario(View view) {
-        String email = edit_email.getText().toString();
-        String senha = edit_senha.getText().toString();
+    private void signInWithGoogle() {
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
 
-        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, senha).addOnCompleteListener(task -> {
-            if (task.isSuccessful()) {
-                progressBar.setVisibility(View.VISIBLE);
-                new Handler().postDelayed(() -> TelaPrincipal(), 3000);
-            } else {
-                String erro;
+    private void AutenticarUsuario(String email, String senha) {
+        progressBar.setVisibility(View.VISIBLE);
 
-                try {
-                    throw Objects.requireNonNull(task.getException());
-                } catch (Exception e) {
-                    erro = "Erro ao logar usuário";
-                }
-                Snackbar snackbar = Snackbar.make(view, erro, Snackbar.LENGTH_SHORT);
-                snackbar.setBackgroundTint(Color.WHITE);
-                snackbar.setTextColor(Color.BLACK);
-                snackbar.show();
-                {
-                }
-            }
-        });
+        mAuth.signInWithEmailAndPassword(email, senha)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull com.google.android.gms.tasks.Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            new Handler().postDelayed(() -> TelaPrincipal(), 3000);
+                        } else {
+                            String erro;
+                            try {
+                                throw Objects.requireNonNull(task.getException());
+                            } catch (Exception e) {
+                                erro = "Erro ao logar usuário";
+                                e.printStackTrace();
+                            }
+                            progressBar.setVisibility(View.INVISIBLE);
+                            Snackbar snackbar = Snackbar.make(findViewById(android.R.id.content), erro, Snackbar.LENGTH_SHORT);
+                            snackbar.setBackgroundTint(Color.WHITE);
+                            snackbar.setTextColor(Color.BLACK);
+                            snackbar.show();
+                        }
+                    }
+                });
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        FirebaseUser user = mAuth.getCurrentUser();
 
         if (user != null) {
             TelaPrincipal();
@@ -99,7 +126,6 @@ public class FormLogin extends AppCompatActivity {
         Intent intent = new Intent(FormLogin.this, TelaPrincipal.class);
         startActivity(intent);
         finish();
-
     }
 
     private void IniciarComponentes() {
@@ -108,6 +134,39 @@ public class FormLogin extends AppCompatActivity {
         edit_senha = findViewById(R.id.edit_senha);
         bt_entrar = findViewById(R.id.bt_entrar);
         progressBar = findViewById(R.id.progressbar);
+        btnGoogleLogin = findViewById(R.id.btnGoogleLogin);
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull com.google.android.gms.tasks.Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            TelaPrincipal();
+                        } else {
+                            Snackbar snackbar = Snackbar.make(findViewById(android.R.id.content), "Erro ao autenticar com o Google", Snackbar.LENGTH_SHORT);
+                            snackbar.setBackgroundTint(Color.WHITE);
+                            snackbar.setTextColor(Color.BLACK);
+                            snackbar.show();
+                        }
+                    }
+                });
     }
 }
--- app/src/main/java/com/example/projetomovimenta/TelaPrincipal.java
+++ app/src/main/java/com/example/projetomovimenta/TelaPrincipal.java
@@ -1,27 +1,30 @@
 package com.example.projetomovimenta;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.example.projetomovimenta.databinding.ActivityFormLoginBinding;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserInfo;
 import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+
+import java.util.Objects;
 
 public class TelaPrincipal extends AppCompatActivity {
 
     private TextView nomeUsuario, emailUsuario;
     private Button bt_deslogar;
-    FirebaseFirestore db=FirebaseFirestore.getInstance();
-    String usuarioID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,38 +32,70 @@ public class TelaPrincipal extends AppCompatActivity {
         setContentView(R.layout.activity_tela_principal);
 
         //getSupportActionBar().hide();
-        iniciarComponentes();
+        initComponents();
 
-        bt_deslogar.setOnClickListener(v -> {
-
-            FirebaseAuth.getInstance().signOut();
-            Intent intent = new Intent(TelaPrincipal.this, FormLogin.class);
-            startActivity(intent);
-            finish();
-        });
+        bt_deslogar.setOnClickListener(this::onClick);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
 
-        String email=FirebaseAuth.getInstance().getCurrentUser().getEmail();
-        usuarioID=FirebaseAuth.getInstance().getCurrentUser().getUid();
+        String email = Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getEmail();
+        String usuarioID = FirebaseAuth.getInstance().getCurrentUser().getUid();
 
-        DocumentReference documentReference= db.collection("Usuários").document(usuarioID);
-        documentReference.addSnapshotListener((documentSnapshot, error) -> {
-            if (documentSnapshot!=null){
-                nomeUsuario.setText(documentSnapshot.getString("nome"));
-                emailUsuario.setText(email);
+        if (FirebaseAuth.getInstance().getCurrentUser().getDisplayName() != null) {
+            nomeUsuario.setText(FirebaseAuth.getInstance().getCurrentUser().getDisplayName());
+            emailUsuario.setText(email);
+        } else {
+
+            DocumentReference documentReference = db.collection("Usuários").document(usuarioID);
+            documentReference.addSnapshotListener((documentSnapshot, error) -> {
+                if (documentSnapshot != null) {
+                    nomeUsuario.setText(documentSnapshot.getString("nome"));
+                    emailUsuario.setText(email);
+                }
+            });
+        }
+    }
+
+    private void initComponents() {
+        nomeUsuario = findViewById(R.id.textNomeUsuario);
+        emailUsuario = findViewById(R.id.textEmailUsuario);
+        bt_deslogar = findViewById(R.id.bt_deslogar);
+    }
+
+    private void onClick(View v) {
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        FirebaseUser user = auth.getCurrentUser();
+
+        assert user != null;
+        for (UserInfo profile : user.getProviderData()) {
+            String providerId = profile.getProviderId();
+
+            if (providerId.equals("google.com")) {
+                GoogleSignIn.getClient(this, GoogleSignInOptions.DEFAULT_SIGN_IN).revokeAccess()
+                        .addOnCompleteListener(this, task -> {
+                            if (task.isSuccessful()) {
+                                Snackbar snackbar = Snackbar.make(v, "Deslogado com sucesso", Snackbar.LENGTH_SHORT);
+                                snackbar.setBackgroundTint(Color.WHITE);
+                                snackbar.setTextColor(Color.BLACK);
+                                snackbar.show();
+                            } else {
+                                Snackbar snackbar = Snackbar.make(v, "Erro ao deslogar usuário", Snackbar.LENGTH_SHORT);
+                                snackbar.setBackgroundTint(Color.WHITE);
+                                snackbar.setTextColor(Color.BLACK);
+                                snackbar.show();
+                            }
+                        });
+
             }
+        }
 
-        });
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(TelaPrincipal.this, FormLogin.class);
+        startActivity(intent);
+        finish();
     }
-
-    private void iniciarComponentes(){
-        nomeUsuario=findViewById(R.id.textNomeUsuario);
-        emailUsuario=findViewById(R.id.textEmailUsuario);
-        bt_deslogar=findViewById(R.id.bt_deslogar);
-    }
-
 }
