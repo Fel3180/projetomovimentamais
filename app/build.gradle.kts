d9d802dc0263446075e007efb1f1f6209cdf482a
Felipe Oliveira <Fel_oliveira_silva@yahoo.com.br>
2023-10-04 20:24:15 -0300

    Sem erros

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,17 +1,16 @@
 plugins {
     id("com.android.application")
     id("com.google.gms.google-services")
-
 }
 
 android {
     namespace = "com.example.projetomovimenta"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.projetomovimenta"
-        minSdk = 21
-        targetSdk = 33
+        minSdk = 24
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
         vectorDrawables.useSupportLibrary = true
@@ -43,19 +42,18 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.firebase:firebase-auth-ktx:22.1.2")
     implementation("com.google.android.gms:play-services-auth:20.7.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
+    implementation("androidx.navigation:navigation-fragment:2.7.3")
+    implementation("androidx.navigation:navigation-ui:2.7.3")
+    implementation("androidx.core:core-ktx:1.12.0")
+
+//    testImplementation("junit:junit:4.13.2")
+//    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+//    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
-    implementation("com.google.firebase:firebase-analytics-ktx")
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom:1.8.0"))
     implementation("com.google.firebase:firebase-auth")
     implementation("com.google.firebase:firebase-firestore")
-// Firebase Authentication
-    implementation ("com.google.firebase:firebase-auth:21.0.1")
-
-// Firebase Firestore (ou outro banco de dados, se estiver usando)
-    implementation ("com.google.firebase:firebase-firestore:23.0.3")
-    implementation ("com.google.firebase:firebase-auth:21.0.1")
-    implementation ("com.google.android.gms:play-services-auth:19.2.0")
 
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,14 @@
 buildscript {
+    repositories {
+        google() // Adicione esta linha
+        mavenCentral()
+    }
     dependencies {
-        classpath("com.google.gms:google-services:4.3.10")
-
+        classpath("com.google.gms:google-services:4.4.0")
+        classpath("com.android.tools.build:gradle:8.1.2")  // Atualize a versão do plugin do Gradle, se necessário
     }
 }
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.1.2" apply false
